b92f623e04870603c5b53460aad894f54536734c
harvey.zh <hyzhai@ucdavis.edu>
2018-07-06 19:31:23 -0700

    style: fix indentation for consistency

--- src/App.js
+++ src/App.js
@@ -4,79 +4,69 @@ import './App.css'
 import BookShelf from './BookShelf';
 
 class BooksApp extends React.Component {
-  // state = {
-  //   /**
-  //    * TODO: Instead of using this state variable to keep track of which page
-  //    * we're on, use the URL in the browser's address bar. This will ensure that
-  //    * users can use the browser's back and forward buttons to navigate between
-  //    * pages, as well as provide a good URL they can bookmark and share.
-  //    */
-  //   showSearchPage: false
-  // }
-  constructor(props) {
-    super(props)
-    this.state = {
-      books: {
-          all: [],
-          currentlyReading: [],
-          wantToRead: [],
-          read: []
-      },
-      
-      showSearchPage: false
+    constructor(props) {
+        super(props)
+        this.state = {
+        books: {
+            all: [],
+            currentlyReading: [],
+            wantToRead: [],
+            read: []
+        },
+        
+        showSearchPage: false
+        }
     }
-  }
 
-  componentDidMount() {
-    BooksAPI.getAll().then((books) => {
-      this.setState({books: {all: books}});
-    });
-  }
+    componentDidMount() {
+        BooksAPI.getAll().then((books) => {
+            this.setState({books: {all: books}});
+        });
+    }
 
-  render() {
-    return (
-      <div className="app">
-        {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+    render() {
+        return (
+            <div className="app">
+                {this.state.showSearchPage ? (
+                <div className="search-books">
+                    <div className="search-books-bar">
+                        <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+                        <div className="search-books-input-wrapper">
+                            {/*
+                            NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                            You can find these search terms here:
+                            https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
+                            However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                            you don't find a specific author or title. Every search is limited by search terms.
+                            */}
+                            <input type="text" placeholder="Search by title or author"/>
+                        </div>
+                    </div>
+                    <div className="search-books-results">
+                        <ol className="books-grid"></ol>
+                    </div>
+                </div>
+                ) : (
+                <div className="list-books">
+                    <div className="list-books-title">
+                        <h1>MyReads</h1>
+                    </div>
+                    <div className="list-books-content">
+                        <div>
+                            <BookShelf shelfTitle="Currently Reading" books={this.state.books.all}/>
+                            <BookShelf shelfTitle="Want to Read" books={this.state.books.all}/>
+                            <BookShelf shelfTitle="Read" books={this.state.books.all}/>
+                        </div>
+                    </div>
+                    <div className="open-search">
+                        <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+                    </div>
+                </div>
+                )}
             </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
-        ) : (
-          <div className="list-books">
-            <div className="list-books-title">
-              <h1>MyReads</h1>
-            </div>
-            <div className="list-books-content">
-              <div>
-                <BookShelf shelfTitle="Currently Reading" books={this.state.books.all}/>
-                <BookShelf shelfTitle="Want to Read" books={this.state.books.all}/>
-                <BookShelf shelfTitle="Read" books={this.state.books.all}/>
-              </div>
-            </div>
-            <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
-            </div>
-          </div>
-        )}
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default BooksApp
