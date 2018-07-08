c0cdaaf2cb1f480a963515c9767df06de1d1d7b6
harvey.zh <hyzhai@ucdavis.edu>
2018-07-07 17:57:20 -0700

    feat: use react router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
 import * as BooksAPI from './BooksAPI';
 import './App.css';
-import BookSearchBar from './BookSearchBar';
+import { Route } from 'react-router-dom'
 import BookLists from './BookLists';
+import BookSearchBar from './BookSearchBar';
 
 class BooksApp extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            books: [],
-            showSearchPage: false
+            books: []
         };
 
+        this.onAddBook = this.onAddBook.bind(this);
         this.onMoveBook = this.onMoveBook.bind(this);
     }
 
@@ -26,20 +27,30 @@ class BooksApp extends React.Component {
         _books.forEach(b => {
             if (b.id === book_id) {
                 b.shelf = dest_shelf;
+                BooksAPI.update(b, dest_shelf);
             }
         });
 
         this.setState({books: _books});
     }
 
+    onAddBook(book_obj, dest_shelf) {
+        book_obj.shelf = dest_shelf;
+        BooksAPI.update(book_obj, dest_shelf);
+        let _books = this.state.books.slice();
+        _books.push(book_obj);
+        this.setState({books: _books});
+    }
+
     render() {
         return (
             <div className="app">
-                {this.state.showSearchPage ? (
+                <Route path='/search' render={() => (
                     <BookSearchBar />
-                ) : (
-                    <BookLists books={this.state.books} onMoveBook={this.onMoveBook}/>
-                )}
+                )}/>
+                <Route exact path='/' render={() => (
+                    <BookLists books={this.state.books} onMoveBook={this.onMoveBook} />
+                )}/>
             </div>
         )
     }
