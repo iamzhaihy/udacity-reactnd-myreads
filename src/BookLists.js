d4b752cff6d694ef4bae618e26d121220a6d3ecb
harvey.zh <hyzhai@ucdavis.edu>
2018-07-08 15:14:42 -0700

    refactor: Convert BookLists to functional component

--- src/BookLists.js
+++ src/BookLists.js
@@ -2,31 +2,29 @@ import React from 'react';
 import BookShelf from './BookShelf';
 import { Link } from 'react-router-dom'
 
-class BookLists extends React.Component {
-    render() {
-        const { books, onMoveBook } = this.props;
-        const currentlyReading = books.filter(b => b.shelf === 'currentlyReading');
-        const wantToRead = books.filter(b => b.shelf === 'wantToRead');
-        const read = books.filter(b => b.shelf === 'read');
+function BookLists(props) {
+    const { books, onMoveBook } = props;
+    const currentlyReading = books.filter(b => b.shelf === 'currentlyReading');
+    const wantToRead = books.filter(b => b.shelf === 'wantToRead');
+    const read = books.filter(b => b.shelf === 'read');
 
-        return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="list-books-content">
-                    <div>
-                        <BookShelf shelfTitle="Currently Reading" books={currentlyReading} onMoveBook={onMoveBook}/>
-                        <BookShelf shelfTitle="Want to Read" books={wantToRead} onMoveBook={onMoveBook}/>
-                        <BookShelf shelfTitle="Read" books={read} onMoveBook={onMoveBook}/>
-                    </div>
-                </div>
-                <div className="open-search">
-                    <Link to='/search'>Add a book</Link>
+    return (
+        <div className="list-books">
+            <div className="list-books-title">
+                <h1>MyReads</h1>
+            </div>
+            <div className="list-books-content">
+                <div>
+                    <BookShelf shelfTitle="Currently Reading" books={currentlyReading} onMoveBook={onMoveBook}/>
+                    <BookShelf shelfTitle="Want to Read" books={wantToRead} onMoveBook={onMoveBook}/>
+                    <BookShelf shelfTitle="Read" books={read} onMoveBook={onMoveBook}/>
                 </div>
             </div>
-        )
-    }
+            <div className="open-search">
+                <Link to='/search'>Add a book</Link>
+            </div>
+        </div>
+    )
 }
 
 export default BookLists;
