390064b0f2fdeefe54bf9c49bb3518acbcdcbbb2
harvey.zh <hyzhai@ucdavis.edu>
2018-07-08 18:33:33 -0700

    feat: Add a new 404 page

---
+++ src/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div style={{textAlign: 'center', fontFamily: 'Courier New, Courier, monospace'}}>
+            <h1 style={{fontSize: '6em'}}>404</h1>
+            <h2 style={{fontSize: '3em'}}>Page Not Found</h2>
+        </div>
+    );
+}
+
+export default NotFound
