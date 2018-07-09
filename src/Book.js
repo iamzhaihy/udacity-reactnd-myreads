0b1a9e3714b970c0143c7ecafa92dc9d94bd44e6
harvey.zh <hyzhai@ucdavis.edu>
2018-07-08 18:17:56 -0700

    refactor: Convert to functional component

--- src/Book.js
+++ src/Book.js
@@ -1,31 +1,30 @@
 import React from 'react';
 
-class Book extends React.Component {
-    render() {
-        const { bookInfo, onMove } = this.props;
-        if (bookInfo.imageLinks) {
-            return (
-                <div className="book">
-                    <div className="book-top">
-                        <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url("${bookInfo.imageLinks.thumbnail}")` }}></div>
-                        <div className="book-shelf-changer">
-                            <select value={bookInfo.shelf} onChange={(event) => onMove(bookInfo, event.target.value)}>
-                                <option value="move" disabled>Move to...</option>
-                                <option value="currentlyReading">Currently Reading</option>
-                                <option value="wantToRead">Want to Read</option>
-                                <option value="read">Read</option>
-                                <option value="none">None</option>
-                            </select>
-                        </div>
-                    </div>
-                    <div className="book-title">{bookInfo.title}</div>
-                    <div className="book-authors">{bookInfo.authors}</div>
+function Book(props) {
+    const { bookInfo, onMove } = props;
+    const { shelf, title, authors, imageLinks} = bookInfo;
+    const cover_link = imageLinks? imageLinks.thumbnail : 'https://imgplaceholder.com/128x193?text=No+Cover+Image';
+    const cover_style = { width: 128, height: 193, backgroundImage: `url("${cover_link}")`};
+
+    return (
+        <div className="book">
+            <div className="book-top">
+                <div className="book-cover" style={cover_style}></div>
+                <div className="book-shelf-changer">
+                    <select value={shelf ? shelf: 'none'} 
+                            onChange={(event) => onMove(bookInfo, event.target.value)}>
+                        <option value="move" disabled>Move to...</option>
+                        <option value="currentlyReading">Currently Reading</option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                    </select>
                 </div>
-            );
-        } else {
-            return null;
-        }
-    }
+            </div>
+            <div className="book-title">{title}</div>
+            <div className="book-authors">{authors}</div>
+        </div>
+    );
 }
 
 export default Book;
--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import Book from './Book';
 
-class BookShelf extends React.Component {
-    render() {
-        const {shelfTitle, books, onMoveBook} = this.props;
-        return (
-            <div className="bookshelf">
-                <h2 className="bookshelf-title">{shelfTitle}</h2>
-                <div className="bookshelf-books">
-                    <ol className="books-grid">
-                        {books && books.map(book => {
-                            return (<li key={book.id}> <Book bookInfo={book} onMove={onMoveBook}/> </li>);
-                        })}
-                    </ol>
-                </div>
+function BookShelf(props) {
+    const {shelfTitle, books, onMoveBook} = props;
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{shelfTitle}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {books && books.map(book => {
+                        return (<li key={book.id}> <Book bookInfo={book} onMove={onMoveBook}/> </li>);
+                    })}
+                </ol>
             </div>
-        )
-    }
+        </div>
+    )
 }
 
 export default BookShelf;
